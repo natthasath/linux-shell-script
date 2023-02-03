d4474a9d0f1f2c9df737cf24dc4cb680132c2ab3
natthasath <natthasath.sak@gmail.com>
2023-02-03 10:30:29 +0700

    Update dmesg-log.sh

--- scripts/dmesg-log.sh
+++ scripts/dmesg-log.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-log_file="dmesg_log_$(date +\%Y\%m\%d_\%H\%M\%S).txt"
+# Store the current date in a variable
+TODAY=$(date +%Y-%m-%d)
 
-# Run dmesg with specified options and redirect output to log file
-dmesg -T --level=emerg,alert,crit,err > $log_file
+# Store the output of the dmesg command in a variable
+LOGS=$(dmesg -T --level=emerg,alert,crit,err)
 
-echo "dmesg log saved to $log_file"
+# Export the logs to a file named "dmesg_logs_$TODAY.txt"
+echo "$LOGS" > dmesg_logs_$TODAY.txt
+
+# Delete logs older than 7 days
+find /path/to/dmesg_logs -type f -mtime +7 -delete
 
 # Prompt the user to enter a path this shell script for cron job
 read -p "Enter the path this shell script for cron job: " path
 
 # Add the command to the crontab
-(crontab -l 2>/dev/null; echo "* * * * * $path") | crontab -
+(crontab -l 2>/dev/null; echo "0 0 * * * $path") | crontab -
